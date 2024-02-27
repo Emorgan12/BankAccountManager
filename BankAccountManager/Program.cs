e6514b451602c53fa1c235feab6caad4be8e783f
Emorgan12 <140021772+Emorgan12@users.noreply.github.com>
2024-02-27 13:48:09 +0000

    sign in created

--- BankAccountManager/Program.cs
+++ BankAccountManager/Program.cs
@@ -5,19 +5,29 @@
         static void Main(string[] args)
         {
             List <string[]> allAccounts = new List<string[]>();
-            Console.WriteLine("Create account [1] or sign-in [2]");
-            string input = Console.ReadLine();
-            if (input == "1")
+            bool exit = false;
+            while (!exit)
             {
-                string[] newAccount = CreateAccount();
-                allAccounts.Add(newAccount);
+                Console.WriteLine("Create account [1], sign-in [2], or exit [3]");
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    string[] newAccount = CreateAccount();
+                    allAccounts.Add(newAccount);
+                }
+                else if (input == "2")
+                {
+                    signIn(allAccounts);
+                }
+                else if (input == "3")
+                {
+                    exit = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
             }
-            else if (input == "2")
-            {
-                //sign into existing account
-            }
-            
-            
             Console.ReadKey();
         }
         static string[] CreateAccount()
@@ -28,6 +38,29 @@
             string password = Console.ReadLine();
             string[] account = new string[] { name, password };
             return account;
+        }
+
+        static void signIn(List<string[]> accountList)
+        {
+            bool found = false;
+            Console.WriteLine("Enter name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
+            foreach (string[] account in accountList)
+            {
+                if (account[0] == name)
+                {
+                    found = true;
+                    Console.WriteLine("Account found... checking password");
+                    if (account[1] == password)
+                        Console.WriteLine("Sign-in successful");
+                    else
+                        Console.WriteLine("Incorrect Password");
+                }
+                if (!found)
+                    Console.WriteLine("Account does not exist");
+            }
         }
     }
 }
