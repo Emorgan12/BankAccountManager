5844bded37f1fca180ccb2e6ea5fd5e962d48d42
Emorgan12 <140021772+Emorgan12@users.noreply.github.com>
2024-02-27 13:01:36 +0000

    account creation (kind of)

--- BankAccountManager/Program.cs
+++ BankAccountManager/Program.cs
@@ -4,16 +4,30 @@
     {
         static void Main(string[] args)
         {
+            List <string[]> allAccounts = new List<string[]>();
             Console.WriteLine("Create account [1] or sign-in [2]");
             string input = Console.ReadLine();
             if (input == "1")
             {
-                //account creation
+                string[] newAccount = CreateAccount();
+                allAccounts.Add(newAccount);
             }
             else if (input == "2")
             {
                 //sign into existing account
             }
+            
+            
+            Console.ReadKey();
+        }
+        static string[] CreateAccount()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Create a password: ");
+            string password = Console.ReadLine();
+            string[] account = new string[] { name, password };
+            return account;
         }
     }
 }
